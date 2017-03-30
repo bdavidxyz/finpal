8f92079fcb245286a47ec76b0d9241fd56a73501
David <davbohr@gmail.com>
2017-03-30 15:42:03 +0200

    sortable

--- app/components/transaction-table.js
+++ app/components/transaction-table.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  sortProperties: ['amount:desc'],
+  sortedTransactions: Ember.computed.sort('transactions', 'sortProperties'),
+
+  actions: {
+
+    sortByDate: function () {
+      this.set('sortProperties', ['date']);
+    },
+
+    sortByCounterparty: function () {
+      this.set('sortProperties', ['counterparty_name']);
+    },
+
+    sortByPayment: function () {
+      this.set('sortProperties', ['operation_type']);
+    },
+
+    sortByAmount: function () {
+      this.set('sortProperties', ['amount']);
+    },
+
+  }
+
 });
