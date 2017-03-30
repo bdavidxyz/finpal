e4c2faf269a09a74502e4725f5466a6ee402b062
David <davbohr@gmail.com>
2017-03-30 14:28:32 +0200

    array on info panel

--- app/components/info-panel.js
+++ app/components/info-panel.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  messageBus: Ember.inject.service('message-bus'),
+
+  selectedTransactions: [],
+
+  init() {
+    this._super(...arguments);
+    this.get('messageBus').subscribe('transaction-item-selected', this, this.transactionSelected);
+    this.get('messageBus').subscribe('transaction-item-unselected', this, this.transactionUnselected);
+  },
+
+  transactionSelected(transaction) {
+    this.get('selectedTransactions').push(transaction);
+  },
+
+  transactionUnselected(transaction) {
+    this.set('selectedTransactions', this.get('selectedTransactions').filter(function( obj ) {
+      return obj.id !== transaction.id;
+    }));
+  }
+
 });
--- app/components/transaction-item.js
+++ app/components/transaction-item.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  messageBus: Ember.inject.service('message-bus'),
   tagName:'',
   isSelected: false,
   actions: {
 
     onSelect: function() {
       this.set('isSelected', !this.get('isSelected'));
+      if (this.get('isSelected')) {
+        this.get('messageBus').publish('transaction-item-selected', this.get('transaction'));
+      } else {
+        this.get('messageBus').publish('transaction-item-unselected', this.get('transaction'));
+      }
+
     },
   }
 });
