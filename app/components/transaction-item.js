306a9c57aa3de2195ea5f7792e7201f51548756e
David <davbohr@gmail.com>
2017-03-30 14:04:51 +0200

    select/unselect

--- app/components/transaction-item.js
+++ app/components/transaction-item.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  tagName:''
+  tagName:'',
+  isSelected: false,
+  actions: {
 
+    onSelect: function() {
+      this.set('isSelected', !this.get('isSelected'));
+    },
+  }
 });
 
